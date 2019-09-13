17e0ea988871adf1b253e25989fd09b6da3c50fa
Victor Fernando Conti <victorfconti@gmail.com>
2019-09-13 19:56:35 -0300

    Criação do projeto

---
+++ src/main/java/br/com/victor/aula_ci/AulaCiApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.aula_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victor/aula_ci/AulaCiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.victor.aula_ci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AulaCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
