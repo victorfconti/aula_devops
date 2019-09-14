f25df93f5a1a385d3a8f7222184f8658a41c459d
Victor Fernando Conti <victorfconti@gmail.com>
2019-09-14 13:41:11 -0300

    Integração do o prometeus

--- src/main/java/br/com/victor/aula_ci/service/SaldoService.java
+++ src/main/java/br/com/victor/aula_ci/service/SaldoService.java
@@ -10,11 +10,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.micrometer.core.annotation.Timed;
+
 @Service
 public class SaldoService {
 	Logger logger = LoggerFactory.getLogger(SaldoService.class);
 	AtomicLong uid = new AtomicLong(1L);
 	
+	@Timed(
+	value = "ufscar_service_saldo",
+	histogram = false,
+	description = "Metodo de obtenção de saldo",
+	extraTags = "Versão 1",
+	percentiles = {0.25, 0.5, 0.95, 0.99})
 	public ResponseEntity<Map<String, Long>> get(){
 		Long saldo = 1200L;
 		logger.info("[UID: {}] Saldo {}", uid, saldo);		
